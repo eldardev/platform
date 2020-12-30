5bd21f319da014364e5208479ee897afb0db84fb
Эльдар Джепаров <abduldzhabbar@gmail.com>
2020-12-31 01:51:24 +0300

    refacroting

--- mvi/src/main/java/com/temel/mvi/viewmodel/StoreViewModel.kt
+++ mvi/src/main/java/com/temel/mvi/viewmodel/StoreViewModel.kt
@@ -4,9 +4,20 @@ import com.temel.mvi.viewstate.Action
 import com.temel.mvi.viewstate.Command
 import com.temel.mvi.viewstate.ViewState
 import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
 
 abstract class StoreViewModel<A : Action, C : Command, VS : ViewState> :
-    ActionViewModel<A, C, VS>() {
+    StateViewModel<VS>() {
+    private val action = BehaviorSubject.create<A>()
+    private val command = BehaviorSubject.create<C>()
+
+    fun dispatch(action: A) {
+        this.action.onNext(action)
+    }
+
+    fun sendCommand(command: C) {
+        this.command.onNext(command)
+    }
 
     abstract fun reduce(action: A, state: VS): VS
 
