63e4c0277d1288498bd0e0301a6cc89497abc099
Эльдар Джепаров <abduldzhabbar@gmail.com>
2021-01-15 23:09:06 +0300

    refactor

--- mvi/src/main/java/com/temel/mvi/viewmodel/StoreViewModel.kt
+++ mvi/src/main/java/com/temel/mvi/viewmodel/StoreViewModel.kt
@@ -11,33 +11,36 @@ abstract class StoreViewModel<A : Action, VS : ViewState> :
 
     private val action = BehaviorSubject.create<A>()
 
+    private lateinit var currentAction: A
+
     fun dispatch(action: A) = this.action.onNext(action)
 
     init {
 
-        this.action.subscribe(
-            { action ->
-                state.value?.let { currentState ->
+        action.distinctUntilChanged()
+            .subscribe(
+                { action ->
+                    state.value?.let { currentState ->
 
-                    val newState = reduceNewState(currentState, action)
-                    mutableState.postValue(newState)
+                        val newState = reduceNewState(currentState, action)
+                        mutableState.postValue(newState)
 
-                    middleWares.forEach {
-                        it(Observable.just(action), currentState).subscribe(
-                            {
-                                this.action.onNext(it)
-                            },
-                            {
-                                this.throwable.postValue(it)
-                            }
-                        ).disposeLater()
+                        middleWares.forEach {
+                            it(Observable.just(action), currentState).subscribe(
+                                {
+                                    this.action.onNext(it)
+                                },
+                                {
+                                    this.throwable.postValue(it)
+                                }
+                            ).disposeLater()
+                        }
                     }
+                },
+                {
+                    throwable.postValue(it)
                 }
-            },
-            {
-                throwable.postValue(it)
-            }
-        ).disposeLater()
+            ).disposeLater()
     }
 
     private fun reduceNewState(currentState: VS, action: A): VS {
@@ -49,7 +52,7 @@ abstract class StoreViewModel<A : Action, VS : ViewState> :
         }
     }
 
-    protected fun setState(state:VS){
+    protected fun setState(state: VS) {
         mutableState.postValue(state)
     }
 }
